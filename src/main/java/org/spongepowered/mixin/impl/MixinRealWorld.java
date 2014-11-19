260898e61f0354680c37d599158a41e13e2ad911
Kobata <kobata2@gmail.com>
2014-11-19 10:09:01 -0500

    Update mixins with new API methods

--- src/main/java/org/spongepowered/mixin/impl/MixinRealWorld.java
+++ src/main/java/org/spongepowered/mixin/impl/MixinRealWorld.java
@@ -24,28 +24,56 @@
  */
 package org.spongepowered.mixin.impl;
 
-import net.minecraft.block.state.IBlockState;
-import net.minecraft.util.BlockPos;
+import com.google.common.base.Optional;
 import net.minecraft.world.WorldProvider;
 import net.minecraft.world.storage.WorldInfo;
 import org.spongepowered.api.block.Block;
+import org.spongepowered.api.entity.Entity;
+import org.spongepowered.api.entity.EntityType;
+import org.spongepowered.api.math.Vector2i;
 import org.spongepowered.api.math.Vector3d;
+import org.spongepowered.api.world.Chunk;
 import org.spongepowered.api.world.World;
+import org.spongepowered.api.world.biome.Biome;
 import org.spongepowered.mod.mixin.Mixin;
 import org.spongepowered.mod.mixin.Shadow;
 import org.spongepowered.wrapper.BlockWrapper;
 
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.UUID;
+
 @Mixin(net.minecraft.world.World.class)
 public abstract class MixinRealWorld implements World {
-
     @Shadow
     public WorldProvider provider;
+
     @Shadow
     protected WorldInfo worldInfo;
 
     @Override
+    public UUID getUniqueID() {
+        return null;
+    }
+
+    @Override
     public String getName() {
         return worldInfo.getWorldName() + "_" + provider.getDimensionName().toLowerCase().replace(' ', '_');
+    }
+
+    @Override
+    public Optional<Chunk> getChunk(Vector2i position) {
+        return Optional.absent();
+    }
+
+    @Override
+    public Optional<Chunk> loadChunk(Vector2i position, boolean shouldGenerate) {
+        return Optional.absent();
+    }
+
+    @Override
+    public Chunk loadChunk(Vector2i position) {
+        return null;
     }
 
     @Override
@@ -56,5 +84,20 @@ public abstract class MixinRealWorld implements World {
     @Override
     public Block getBlock(int x, int y, int z) {
         return new BlockWrapper(this, x, y, z);
+    }
+
+    @Override
+    public Biome getBiome(Vector3d position) {
+        return null;
+    }
+
+    @Override
+    public Collection<Entity> getEntities() {
+        return new ArrayList<Entity>();
+    }
+
+    @Override
+    public Optional<Entity> createEntity(EntityType type, Vector3d position) {
+        return Optional.absent();
     }
 }
