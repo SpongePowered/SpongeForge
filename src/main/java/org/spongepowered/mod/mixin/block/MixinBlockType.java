10f6efdafcb7cf994f5a492951d58922b18b38dc
Nick Conde <nick@budgiehouse.com>
2014-12-20 01:01:31 -0800

    Impl of Block/Item expansion

--- src/main/java/org/spongepowered/mod/mixin/block/MixinBlockType.java
+++ src/main/java/org/spongepowered/mod/mixin/block/MixinBlockType.java
@@ -25,6 +25,8 @@
 package org.spongepowered.mod.mixin.block;
 
 import net.minecraft.block.Block;
+import net.minecraft.block.BlockFalling;
+import net.minecraft.block.BlockLiquid;
 import net.minecraft.block.state.IBlockState;
 
 import org.spongepowered.api.block.BlockState;
@@ -49,6 +51,15 @@ public abstract class MixinBlockType implements BlockType {
     public abstract String shadow$getUnlocalizedName();
 
     @Shadow
+    public abstract boolean isSolidFullCube();
+
+    @Shadow
+    public abstract boolean getEnableStats();
+
+    @Shadow
+    public abstract int getLightValue();
+
+    @Shadow
     public abstract IBlockState getStateFromMeta(int meta);
 
     @Override
@@ -71,4 +82,30 @@ public abstract class MixinBlockType implements BlockType {
     public BlockState getStateFromDataValue(byte data) {
         return (BlockState) getStateFromMeta(data);
     }
+
+    @Override
+    public boolean isLiquid() {
+        return BlockLiquid.class.isAssignableFrom(this.getClass());
+    }
+
+    @Override
+    public boolean isSolidCube() {
+        return isSolidFullCube();
+    }
+
+    @Override
+    public boolean isAffectedByGravity() {
+        return BlockFalling.class.isAssignableFrom(this.getClass());
+    }
+
+    @Override
+    public boolean areStatisticsEnabled() {
+        return getEnableStats();
+    }
+
+    @Override
+    public float getEmittedLight() {
+        return 15F / getLightValue();
+    }
+
 }
