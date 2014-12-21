9e6795cd5be4d06f7a279eddf04a0c6f68c1d767
jack9515 <jack@rickard.plus.com>
2014-12-21 18:39:46 +0000

    Fixed IRC raised issues

--- src/main/java/org/spongepowered/mod/mixin/item/MixinEnchantment.java
+++ src/main/java/org/spongepowered/mod/mixin/item/MixinEnchantment.java
@@ -25,6 +25,7 @@
 package org.spongepowered.mod.mixin.item;
 
 import java.util.Map;
+import java.util.Map.Entry;
 
 import net.minecraft.util.ResourceLocation;
 
@@ -35,79 +36,81 @@ import org.spongepowered.api.util.annotation.NonnullByDefault;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 
-import com.google.common.collect.BiMap;
-import com.google.common.collect.HashBiMap;
 import com.google.common.collect.Maps;
 
 @NonnullByDefault
 @Mixin(net.minecraft.enchantment.Enchantment.class)
 public abstract class MixinEnchantment implements Enchantment {
 
-    @Shadow(prefix = "shadow$") private static final Map<ResourceLocation, Enchantment> shadow$field_180307_E = Maps.newHashMap();
+    @Shadow private static final Map<ResourceLocation, net.minecraft.enchantment.Enchantment> field_180307_E = Maps.newHashMap();
 
-    @Shadow(prefix = "shadow$")
-    public abstract int shadow$getMinLevel();
+    @Shadow
+    public abstract int getMinLevel();
 
-    @Shadow(prefix = "shadow$")
-    public abstract int shadow$getMaxLevel();
+    @Shadow
+    public abstract int getMaxLevel();
 
-    @Shadow(prefix = "shadow$")
-    public abstract int shadow$getMinEnchantability(int level);
+    @Shadow
+    public abstract int getMinEnchantability(int level);
 
-    @Shadow(prefix = "shadow$")
-    public abstract int shadow$getMaxEnchantability(int level);
+    @Shadow
+    public abstract int getMaxEnchantability(int level);
 
-    @Shadow(prefix = "shadow$")
-    public abstract boolean shadow$canApplyAtEnchantingTable(net.minecraft.item.ItemStack stack);
+    @Shadow
+    public abstract boolean canApplyAtEnchantingTable(net.minecraft.item.ItemStack stack);
 
-    @Shadow(prefix = "shadow$")
-    public abstract boolean shadow$canApplyTogether(net.minecraft.enchantment.Enchantment ench);
+    @Shadow
+    public abstract boolean canApplyTogether(net.minecraft.enchantment.Enchantment ench);
 
-    @Shadow(prefix = "shadow$")
-    public abstract boolean shadow$canApply(net.minecraft.item.ItemStack stack);
+    @Shadow
+    public abstract boolean canApply(net.minecraft.item.ItemStack stack);
 
-    @Shadow(prefix = "shadow$")
-    public abstract boolean shadow$isAllowedOnBooks();
-
-    private static final BiMap<Enchantment, ResourceLocation> ids = HashBiMap.create(shadow$field_180307_E).inverse();
+    @Shadow
+    public abstract boolean isAllowedOnBooks();
 
     @Override
     public String getId() {
-        return ids.get(this).toString();
+        for(Entry<ResourceLocation, net.minecraft.enchantment.Enchantment> entry : field_180307_E.entrySet())
+        {
+            if (entry.getValue().equals(this)) {
+                return entry.getKey().toString();
+            }
+        }
+        return null;
     }
 
     @Override
     public int getMinimumLevel() {
-        return shadow$getMinLevel();
+        return getMinLevel();
     }
 
     @Override
     public int getMaximumLevel() {
-        return shadow$getMaxLevel();
+        return getMaxLevel();
     }
 
     @Override
     public int getMinimumEnchantabilityForLevel(int level) {
-        return shadow$getMinEnchantability(level);
+        return getMinEnchantability(level);
     }
 
     @Override
     public int getMaximumEnchantabilityForLevel(int level) {
-        return shadow$getMaxEnchantability(level);
+        return getMaxEnchantability(level);
     }
 
     @Override
     public boolean canBeAppliedToStack(ItemStack stack) {
-        return (stack.getItem() == ItemTypes.BOOK) ? shadow$isAllowedOnBooks() : shadow$canApply((net.minecraft.item.ItemStack) stack);
+        return (stack.getItem() == ItemTypes.BOOK) ? isAllowedOnBooks() : canApply((net.minecraft.item.ItemStack) stack);
     }
 
     @Override
     public boolean canBeAppliedByTable(ItemStack stack) {
-        return shadow$canApplyAtEnchantingTable((net.minecraft.item.ItemStack) stack);
+        return canApplyAtEnchantingTable((net.minecraft.item.ItemStack) stack);
     }
 
     @Override
     public boolean isCompatibleWith(Enchantment ench) {
-        return shadow$canApplyTogether((net.minecraft.enchantment.Enchantment) ench);
+        return canApplyTogether((net.minecraft.enchantment.Enchantment) ench);
     }
 }
