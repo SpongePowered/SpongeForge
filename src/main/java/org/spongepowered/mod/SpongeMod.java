d3adfea38cd1e37239699c315d5f0ebd98de0050
sibomots <sibomots@outlook.com>
2015-01-15 21:08:31 -0800

    Asynchronous Scheduler

--- src/main/java/org/spongepowered/mod/SpongeMod.java
+++ src/main/java/org/spongepowered/mod/SpongeMod.java
@@ -128,7 +128,7 @@ public class SpongeMod extends DummyModContainer {
         MinecraftForge.EVENT_BUS.register(new SpongeEventHooks());
 
         // Add the SyncScheduler as a listener for ServerTickEvents
-        FMLCommonHandler.instance().bus().register(this.getGame().getScheduler());
+        FMLCommonHandler.instance().bus().register(this.getGame().getSyncScheduler());
     }
 
     @Subscribe
