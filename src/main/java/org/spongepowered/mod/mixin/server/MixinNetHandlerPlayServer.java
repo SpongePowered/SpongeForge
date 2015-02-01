252e232cbc0f638c2dc90d60c87edd9004e50b7f
verbeto <tnmjimbob@gmail.com>
2015-02-01 17:28:58 +0000

    Update MixinNetHandlerPlayServer.java
    Added functionality for tab complete.

--- src/main/java/org/spongepowered/mod/mixin/server/MixinNetHandlerPlayServer.java
+++ src/main/java/org/spongepowered/mod/mixin/server/MixinNetHandlerPlayServer.java
@@ -25,27 +25,42 @@
 package org.spongepowered.mod.mixin.server;
 
 import io.netty.buffer.Unpooled;
+import net.minecraft.command.ICommandSender;
 import net.minecraft.entity.player.EntityPlayerMP;
 import net.minecraft.network.NetHandlerPlayServer;
 import net.minecraft.network.NetworkManager;
 import net.minecraft.network.Packet;
 import net.minecraft.network.PacketBuffer;
+import net.minecraft.network.PacketThreadUtil;
+import net.minecraft.network.play.INetHandlerPlayServer;
+import net.minecraft.network.play.client.C14PacketTabComplete;
+import net.minecraft.network.play.server.S3APacketTabComplete;
 import net.minecraft.network.play.server.S3FPacketCustomPayload;
+import net.minecraft.server.MinecraftServer;
+
 import org.spongepowered.api.entity.player.Player;
 import org.spongepowered.api.net.ChannelBuf;
 import org.spongepowered.api.net.PlayerConnection;
+import org.spongepowered.api.util.command.CommandSource;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.mod.SpongeMod;
 import org.spongepowered.mod.server.ConnectionInfo;
 
+import com.google.common.collect.Lists;
+
 import java.net.InetSocketAddress;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
 
 @Mixin(NetHandlerPlayServer.class)
-public abstract class MixinNetHandlerPlayServer implements PlayerConnection {
+public abstract class MixinNetHandlerPlayServer implements PlayerConnection, INetHandlerPlayServer {
 
     @Shadow public NetworkManager netManager;
     @Shadow public EntityPlayerMP playerEntity;
-
+    @Shadow private MinecraftServer serverController;
+    
     @Shadow
     public abstract void sendPacket(final Packet packetIn);
 
@@ -79,4 +94,57 @@ public abstract class MixinNetHandlerPlayServer implements PlayerConnection {
         sendPacket(new S3FPacketCustomPayload(channel, new PacketBuffer(Unpooled.wrappedBuffer(data))));
     }
 
+    @Override
+	public void processTabComplete(C14PacketTabComplete packetIn) {
+		PacketThreadUtil.func_180031_a(packetIn, this, this.playerEntity.getServerForPlayer());
+		ArrayList<String> arraylist = Lists.newArrayList();
+
+		Iterator<?> iterator = processTab(playerEntity, packetIn.getMessage()).iterator();
+
+		while (iterator.hasNext()) {
+			String value = (String) iterator.next();
+			arraylist.add(value);
+		}
+
+		Iterator<?> vanillaIterator = this.serverController.func_180506_a(this.playerEntity, packetIn.getMessage(), packetIn.func_179709_b()).iterator();
+
+		while (vanillaIterator.hasNext()) {
+			String value = (String) vanillaIterator.next();
+			arraylist.add(value);
+		}
+		this.playerEntity.playerNetServerHandler.sendPacket(new S3APacketTabComplete((String[]) arraylist.toArray(new String[arraylist.size()])));
+
+	}
+
+	@SuppressWarnings({ "rawtypes", "unchecked" })
+	private List<String> processTab(ICommandSender sender, String string) {
+
+		ArrayList arraylist = Lists.newArrayList();
+
+		if (string.startsWith("/")) {
+			string = string.substring(1);
+			boolean flag = !string.contains(" ");
+			List list;
+			try {
+				list = SpongeMod.instance.getGame().getCommandDispatcher().getSuggestions((CommandSource) sender, string);
+				if (list != null) {
+					Iterator iterator = list.iterator();
+
+					while (iterator.hasNext()) {
+						String value = (String) iterator.next();
+
+						if (flag) {
+							arraylist.add("/" + value);
+						} else {
+							arraylist.add(value);
+						}
+					}
+				}
+			} catch (Exception e) {
+			}
+
+		}
+		return arraylist;
+	}
+    
 }
