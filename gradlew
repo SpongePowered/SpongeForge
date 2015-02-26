88f33f239bb21d503b308952fd7092ff80ab73b3
Kenzie Togami <ket1999@gmail.com>
2015-02-25 22:41:38 -0800

    Update wrapper scripts with friendly messages

--- gradlew
+++ gradlew
@@ -1,1 +1,5 @@
+if [ ! -f ./SpongeAPI/gradlew ]; then
+    echo "Error locating the Gradle subprojects. You most likely forgot to run 'git submodule update --init'. This usually doesn't happen to people who read the README ;)"
+    exit 1
+fi
 ./SpongeAPI/gradlew $@
--- gradlew.bat
+++ gradlew.bat
@@ -1,1 +1,6 @@
+@ECHO off
+IF NOT EXIST SpongeAPI\gradlew.bat (
+    echo Error locating the Gradle subprojects. You most likely forgot to run 'git submodule update --init'. This usually doesn't happen to people who read the README ;^)
+    exit 1
+)
 SpongeAPI\gradlew.bat %*
