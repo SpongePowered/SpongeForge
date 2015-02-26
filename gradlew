6b35c42e0c89615ada0b5e7bc8c2febbfd41506c
gratimax <max@ovsankin.com>
2015-02-25 22:20:57 -0800

    Friendlier error messages when building. Closes #183.

--- gradlew
+++ gradlew
@@ -1,1 +1,8 @@
+if [ ! -f ./SpongeAPI/gradlew ]; then
+    echo "Error locating the Gradle subprojects. You most likely forgot to run 'git submodule update --init'. This usually doesn't happen to people who read the README ;)"
+    # run it for them
+    git submodule update --init --recursive
+    # also assume they didn't run the cp
+    cp scripts/pre-commit .git/hooks
+fi
 ./SpongeAPI/gradlew $@
--- gradlew.bat
+++ gradlew.bat
@@ -1,1 +1,9 @@
+@ECHO off
+IF NOT EXIST SpongeAPI\gradlew.bat (
+    echo Error locating the Gradle subprojects. You most likely forgot to run 'git submodule update --init'. This usually doesn't happen to people who read the README ;^)
+    REM run it for them
+    git submodule update --init --recursive
+    REM also assume they didn't run the cp
+    cp scripts/pre-commit .git/hooks
+)
 SpongeAPI\gradlew.bat %*
