292f1d3091327d7c5b538e8c232d68ec11f6ab63
Me4502 <mnmiller1@hotmail.com>
2015-03-09 16:32:24 +1000

    Implement BlockUpdateEvent.

---
+++ src/main/java/org/spongepowered/mod/mixin/core/event/block/MixinBlockUpdateEvent.java
@@ -0,0 +1,67 @@
+/*
+ * This file is part of Sponge, licensed under the MIT License (MIT).
+ *
+ * Copyright (c) SpongePowered.org <http://www.spongepowered.org>
+ * Copyright (c) contributors
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+package org.spongepowered.mod.mixin.core.event.block;
+
+import net.minecraft.block.state.IBlockState;
+import net.minecraft.util.BlockPos;
+import net.minecraft.util.EnumFacing;
+import net.minecraft.world.World;
+import net.minecraftforge.event.world.BlockEvent;
+import org.spongepowered.api.block.BlockLoc;
+import org.spongepowered.api.event.block.BlockUpdateEvent;
+import org.spongepowered.api.util.annotation.NonnullByDefault;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+
+import java.util.Collection;
+import java.util.EnumSet;
+import java.util.HashSet;
+import java.util.Set;
+
+@NonnullByDefault
+@Mixin(value = BlockEvent.NeighborNotifyEvent.class, remap = false)
+public abstract class MixinBlockUpdateEvent extends BlockEvent implements BlockUpdateEvent {
+
+    @Shadow
+    private final EnumSet<EnumFacing> notifiedSides = null;
+
+    private final Set<BlockLoc> affectedBlocks;
+
+    public MixinBlockUpdateEvent(World world, BlockPos pos, IBlockState state) {
+        super(world, pos, state);
+
+        affectedBlocks = new HashSet<BlockLoc>();
+        for(EnumFacing notifiedSide : notifiedSides) {
+            BlockPos offset = pos.offset(notifiedSide);
+            affectedBlocks.add(((org.spongepowered.api.world.World) world).getFullBlock(offset.getX(), offset.getY(), offset.getZ()));
+        }
+    }
+
+    @Override
+    public Collection<BlockLoc> getAffectedBlocks() {
+        return affectedBlocks;
+    }
+
+}
