bd07085cf983e5024b395e249c647f6465c1295c
Aaron Hill <aa1ronham@gmail.com>
2015-10-17 23:01:11 -0400

    Remove Ivy launch handlers
    To use Sponge, you must now download and install Forge
    separately.

--- src/main/java/org/spongepowered/launch/Main.java
+++ src/main/java/org/spongepowered/launch/Main.java
@@ -25,23 +25,11 @@
 package org.spongepowered.launch;
 
 import org.spongepowered.launch.gui.LaunchInfoPanel;
-import org.spongepowered.launch.handlers.GoHandler;
-import org.spongepowered.launch.handlers.HelpHandler;
-import org.spongepowered.launch.handlers.ResolveHandler;
-import org.spongepowered.launch.handlers.RunHandler;
 
 import java.io.File;
 import java.io.IOException;
 import java.lang.reflect.Method;
 import java.net.URI;
-import java.net.URL;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collection;
-import java.util.Collections;
-import java.util.LinkedHashMap;
-import java.util.List;
-import java.util.Map;
 import java.util.Scanner;
 import java.util.jar.Attributes;
 import java.util.jar.JarFile;
@@ -60,29 +48,17 @@ public class Main {
 
     private static final File JAR = Main.getJar();
     public static final String JAR_NAME = Main.getJarName();
-    
-    private static final URL IVY_SETTINGS_RESOURCE = Main.class.getResource("ivysettings.xml");
-    private static final URL IVY_RESOURCE = Main.class.getResource("ivy.xml");
-    
-    private static Attributes manifestAttributes = null; 
-    
-    private static final Map<String, LaunchHandler> handlers = new LinkedHashMap<String, LaunchHandler>();
-    
+
+    private static Attributes manifestAttributes = null;
+
     public static void main(String[] argv) {
         if (System.console() == null && !Main.checkHeadlessState()) {
             Main.displayGraphicalMessage();
             return;
         }
-            
+
         Main.printHeader();
-        
-        List<String> args = new ArrayList<String>(Arrays.asList(argv));
-        Main.initDefaultHandlers();
-        try {
-            Main.dispatch(args);
-        } catch (Throwable th) {
-            th.printStackTrace();
-        }
+        Main.printUsage();
     }
 
     private static void displayGraphicalMessage() {
@@ -122,44 +98,9 @@ public class Main {
         return true;
     }
 
-    private static void dispatch(List<String> args) {
-        if (args.size() > 0) {
-            String command = Main.getCommand(args);
-            LaunchHandler handler = Main.handlers.get(command);
-            if (handler == null) {
-                System.out.printf("\nAn invalid option was specified!\n");
-                handler = Main.handlers.get("help");
-            }
-            handler.acceptArguments(args);
-            handler.run();
-        } else {
-            Main.printUsage();
-        }
-    }
-
-    private static String getCommand(List<String> args) {
-        String command = args.remove(0).toLowerCase();
-        return command.startsWith("--") ? command.substring(2) : command;
-    }
-
     private static void printUsage() {
         Scanner help = new Scanner(Main.class.getResourceAsStream("help.txt")).useDelimiter("\\Z");
         System.out.print(help.next().replace("${jarfile}", Main.JAR_NAME));
-    }
-
-    private static void initDefaultHandlers() {
-        Main.addHandler(new HelpHandler());
-        Main.addHandler(new ResolveHandler(Main.IVY_SETTINGS_RESOURCE, Main.IVY_RESOURCE));
-        Main.addHandler(new RunHandler(Main.IVY_SETTINGS_RESOURCE, Main.IVY_RESOURCE));
-        Main.addHandler(new GoHandler(Main.IVY_SETTINGS_RESOURCE, Main.IVY_RESOURCE));
-    }
-
-    public static void addHandler(LaunchHandler handler) {
-        Main.handlers.put(handler.getName().toLowerCase(), handler);
-    }
-    
-    public static Collection<LaunchHandler> getHandlers() {
-        return Collections.unmodifiableCollection(Main.handlers.values());
     }
 
     private static File getJar() {
@@ -184,10 +125,10 @@ public class Main {
         } catch (Exception ex) {
             // derp
         }
-        
+
         return null;
     }
-    
+
     private static String getJarName() {
         return Main.JAR != null ? Main.JAR.getName() : "sponge.jar";
     }
@@ -196,7 +137,7 @@ public class Main {
         if (Main.JAR == null) {
             return defaultValue;
         }
-        
+
         Attributes manifestAttributes = Main.getManifestAttributes();
         return manifestAttributes != null ? manifestAttributes.getValue(key) : defaultValue;
     }
@@ -207,7 +148,7 @@ public class Main {
             try {
                 jarFile = new JarFile(Main.JAR);
                 Main.manifestAttributes = jarFile.getManifest().getMainAttributes();
-                
+
             } catch (IOException ex) {
                 // be quiet checkstyle
             } finally {
@@ -220,7 +161,7 @@ public class Main {
                 }
             }
         }
-        
+
         return Main.manifestAttributes;
     }
 
