b834199a150ef7a7f4e8274aecfb6495b686874f
Aaron Hill <aa1ronham@gmail.com>
2015-10-17 22:28:37 -0400

    Update weather mixin

--- src/main/java/org/spongepowered/mod/mixin/core/world/MixinWorld.java
+++ src/main/java/org/spongepowered/mod/mixin/core/world/MixinWorld.java
@@ -30,24 +30,19 @@ import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import org.spongepowered.common.interfaces.IMixinWorld;
 
 @Mixin(value = net.minecraft.world.World.class, priority = 1001)
-public abstract class MixinWorld implements org.spongepowered.api.world.World {
+public abstract class MixinWorld implements org.spongepowered.api.world.World, IMixinWorld {
 
     @Shadow public WorldInfo worldInfo;
-    private long weatherStartTime;
 
     @Inject(method = "updateWeatherBody()V", remap = false, at = {
             @At(value = "INVOKE", target = "Lnet/minecraft/world/storage/WorldInfo;setThundering(Z)V"),
             @At(value = "INVOKE", target = "Lnet/minecraft/world/storage/WorldInfo;setRaining(Z)V")
     })
     private void onUpdateWeatherBody(CallbackInfo ci) {
-        this.weatherStartTime = this.worldInfo.getWorldTotalTime();
-    }
-
-    @Override
-    public long getRunningDuration() {
-        return this.worldInfo.getWorldTotalTime() - this.weatherStartTime;
+        this.setWeatherStartTime(this.worldInfo.getWorldTotalTime());
     }
 
 }
