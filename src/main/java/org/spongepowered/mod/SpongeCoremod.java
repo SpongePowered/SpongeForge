fad9e317be3d1e169d8a66de9a3b39f0c912fcf6
simon816 <simonbarnes1@googlemail.com>
2015-10-24 01:47:39 +0100

    Bump common ref
    Add timings mixin config to launch

--- src/main/java/org/spongepowered/mod/SpongeCoremod.java
+++ src/main/java/org/spongepowered/mod/SpongeCoremod.java
@@ -82,6 +82,7 @@ public class SpongeCoremod implements IFMLLoadingPlugin {
                 .addConfiguration("mixins.common.api.json")
                 .addConfiguration("mixins.common.core.json")
                 .addConfiguration("mixins.common.bungeecord.json")
+                .addConfiguration("mixins.common.timings.json")
                 .addConfiguration("mixins.forge.core.json")
                 .addConfiguration("mixins.forge.entityactivation.json")
                 .registerTokenProviderClass("org.spongepowered.mod.SpongeCoremod$TokenProvider");
