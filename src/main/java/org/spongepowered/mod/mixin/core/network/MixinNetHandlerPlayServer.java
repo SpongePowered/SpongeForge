92a6178cf5c196f59d983ec508c771155fcfeb99
bloodshot <jdroque@gmail.com>
2015-12-14 19:51:53 -0500

    Don't run item use part of hook if PlayerInteractEvent is cancelled.
    Fixes SpongePowered/SpongeCommon#327

--- src/main/java/org/spongepowered/mod/mixin/core/network/MixinNetHandlerPlayServer.java
+++ src/main/java/org/spongepowered/mod/mixin/core/network/MixinNetHandlerPlayServer.java
@@ -25,15 +25,21 @@
 package org.spongepowered.mod.mixin.core.network;
 
 import com.google.common.collect.Sets;
+import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.entity.player.EntityPlayerMP;
+import net.minecraft.item.ItemStack;
 import net.minecraft.network.NetHandlerPlayServer;
 import net.minecraft.network.NetworkManager;
 import net.minecraft.network.Packet;
 import net.minecraft.network.play.client.C01PacketChatMessage;
 import net.minecraft.server.MinecraftServer;
+import net.minecraft.server.management.ItemInWorldManager;
+import net.minecraft.util.BlockPos;
 import net.minecraft.util.ChatComponentTranslation;
+import net.minecraft.util.EnumFacing;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.event.ServerChatEvent;
+import net.minecraftforge.event.entity.player.PlayerInteractEvent;
 import org.apache.logging.log4j.Logger;
 import org.spongepowered.api.event.command.MessageSinkEvent;
 import org.spongepowered.api.text.Text;
@@ -42,14 +48,16 @@ import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.Redirect;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import org.spongepowered.asm.mixin.injection.callback.LocalCapture;
+import org.spongepowered.mod.event.SpongeForgeEventFactory;
 import org.spongepowered.mod.interfaces.IMixinEventPlayerChat;
 import org.spongepowered.mod.interfaces.IMixinNetPlayHandler;
 
 import java.util.Set;
 
-@Mixin(NetHandlerPlayServer.class)
+@Mixin(value = NetHandlerPlayServer.class, priority = 1001)
 public abstract class MixinNetHandlerPlayServer implements IMixinNetPlayHandler {
 
     @Shadow public EntityPlayerMP playerEntity;
@@ -93,4 +101,16 @@ public abstract class MixinNetHandlerPlayServer implements IMixinNetPlayHandler 
         return this.registeredChannels;
     }
 
+    @Redirect(method = "processPlayerBlockPlacement", at = @At(value = "INVOKE", target="Lnet/minecraft/server/management/ItemInWorldManager;activateBlockOrUseItem(Lnet/minecraft/entity/player/EntityPlayer;Lnet/minecraft/world/World;Lnet/minecraft/item/ItemStack;Lnet/minecraft/util/BlockPos;Lnet/minecraft/util/EnumFacing;FFF)Z"))
+    public boolean onActivateBlockOrUseItem(ItemInWorldManager itemManager, EntityPlayer player, net.minecraft.world.World worldIn, ItemStack stack, BlockPos pos, EnumFacing side, float hitX, float hitY, float hitZ) {
+        boolean result = itemManager.activateBlockOrUseItem(player, worldIn, stack, pos, side, hitX, hitY, hitZ);
+        if (stack != null && !result) {
+            // Don't run item use part of hook if PlayerInteractEvent was cancelled.
+            if (SpongeForgeEventFactory.lastForgeEvent instanceof PlayerInteractEvent && SpongeForgeEventFactory.lastForgeEvent.isCanceled()) {
+                return false;
+            }
+            itemManager.tryUseItem(player, worldIn, stack);
+        }
+        return result;
+    }
 }
