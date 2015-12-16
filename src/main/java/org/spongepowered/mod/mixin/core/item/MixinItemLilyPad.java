540d00c267e673040c1d1ae9ed7873a089d5b029
bloodshot <jdroque@gmail.com>
2015-12-16 11:34:30 -0500

    Fix crash placing water lilies on client.
    * Update SpongeCommon ref.

---
+++ src/main/java/org/spongepowered/mod/mixin/core/item/MixinItemLilyPad.java
@@ -0,0 +1,93 @@
+/*
+ * This file is part of Sponge, licensed under the MIT License (MIT).
+ *
+ * Copyright (c) SpongePowered <https://www.spongepowered.org>
+ * Copyright (c) contributors
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+package org.spongepowered.mod.mixin.core.item;
+
+import net.minecraft.block.Block;
+import net.minecraft.block.BlockLiquid;
+import net.minecraft.block.material.Material;
+import net.minecraft.block.state.IBlockState;
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.init.Blocks;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemColored;
+import net.minecraft.item.ItemLilyPad;
+import net.minecraft.item.ItemStack;
+import net.minecraft.stats.StatList;
+import net.minecraft.util.BlockPos;
+import net.minecraft.util.MovingObjectPosition;
+import net.minecraft.world.World;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Overwrite;
+
+@Mixin(ItemLilyPad.class)
+public abstract class MixinItemLilyPad extends ItemColored {
+
+    public MixinItemLilyPad(Block block, boolean hasSubtypes) {
+        super(block, hasSubtypes);
+    }
+
+    /**
+     * @author bloodmc - December 16th, 2015
+     *
+     *         Purpose: Restored to vanilla method as we do not need Forge's
+     *         block snapshot calls nor event call due to our tracking system.
+     */
+    @Overwrite
+    public ItemStack onItemRightClick(ItemStack itemStackIn, World worldIn, EntityPlayer playerIn) {
+        MovingObjectPosition movingobjectposition = this.getMovingObjectPositionFromPlayer(worldIn, playerIn, true);
+
+        if (movingobjectposition == null) {
+            return itemStackIn;
+        } else {
+            if (movingobjectposition.typeOfHit == MovingObjectPosition.MovingObjectType.BLOCK) {
+                BlockPos blockpos = movingobjectposition.getBlockPos();
+
+                if (!worldIn.isBlockModifiable(playerIn, blockpos)) {
+                    return itemStackIn;
+                }
+
+                if (!playerIn.canPlayerEdit(blockpos.offset(movingobjectposition.sideHit), movingobjectposition.sideHit, itemStackIn)) {
+                    return itemStackIn;
+                }
+
+                BlockPos blockpos1 = blockpos.up();
+                IBlockState iblockstate = worldIn.getBlockState(blockpos);
+
+                if (iblockstate.getBlock().getMaterial() == Material.water && ((Integer) iblockstate.getValue(BlockLiquid.LEVEL)).intValue() == 0
+                        && worldIn.isAirBlock(blockpos1)) {
+                    worldIn.setBlockState(blockpos1, Blocks.waterlily.getDefaultState());
+
+                    if (!playerIn.capabilities.isCreativeMode) {
+                        --itemStackIn.stackSize;
+                    }
+
+                    playerIn.triggerAchievement(StatList.objectUseStats[Item.getIdFromItem(this)]);
+                }
+            }
+
+            return itemStackIn;
+        }
+    }
+}
