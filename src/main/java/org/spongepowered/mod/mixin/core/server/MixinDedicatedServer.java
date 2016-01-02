ffaef88c38cc8d1cb322d4e12210fea238975074
Minecrell <dev@minecrell.net>
2016-01-02 17:27:38 +0100

    Fix console not showing any colors
    TODO: Remove once fixed in Forge

---
+++ src/main/java/org/spongepowered/mod/mixin/core/server/MixinDedicatedServer.java
@@ -0,0 +1,54 @@
+/*
+ * This file is part of Sponge, licensed under the MIT License (MIT).
+ *
+ * Copyright (c) SpongePowered <https://www.spongepowered.org>
+ * Copyright (c) contributors
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+package org.spongepowered.mod.mixin.core.server;
+
+import net.minecraft.server.MinecraftServer;
+import net.minecraft.server.dedicated.DedicatedServer;
+import net.minecraft.util.IChatComponent;
+import org.apache.logging.log4j.Logger;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.common.text.SpongeTexts;
+
+import java.io.File;
+import java.net.Proxy;
+
+@Mixin(DedicatedServer.class)
+public abstract class MixinDedicatedServer extends MinecraftServer {
+
+    @Shadow private static Logger logger;
+
+    private MixinDedicatedServer(Proxy proxy, File workDir) {
+        super(proxy, workDir);
+    }
+
+    // Fix colors in console
+    // TODO: Remove once fixed in Forge
+    @Override
+    public void addChatMessage(IChatComponent component) {
+        logger.info(SpongeTexts.toLegacy(component));
+    }
+
+}
