339f21c100a2afd7ebb1c7dc199e0e04ef9bab6d
Aaron Hill <aa1ronham@gmail.com>
2016-03-23 22:48:05 -0400

    Mix in client-only methods to SpongeProxyBlockAccess

---
+++ src/main/java/org/spongepowered/mod/mixin/core/common/MixinSpongeProxyBlockAccess.java
@@ -0,0 +1,61 @@
+/*
+ * This file is part of Sponge, licensed under the MIT License (MIT).
+ *
+ * Copyright (c) SpongePowered <https://www.spongepowered.org>
+ * Copyright (c) contributors
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+package org.spongepowered.mod.mixin.core.common;
+
+import net.minecraft.util.BlockPos;
+import net.minecraft.world.IBlockAccess;
+import net.minecraft.world.WorldType;
+import net.minecraft.world.biome.BiomeGenBase;
+import org.spongepowered.asm.mixin.Final;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.common.world.SpongeProxyBlockAccess;
+
+@Mixin(SpongeProxyBlockAccess.class)
+public class MixinSpongeProxyBlockAccess {
+
+    @Shadow @Final IBlockAccess original;
+
+    // These methods are @SideOnly(Side.CLIENT)
+    // We only mix them in in SpongeForge, so that mod blocks
+    // don't break when trying to call these methods on the SpongeProxyBlockAccess
+    // we pass in
+
+    public int getCombinedLight(BlockPos pos, int lightValue) {
+        return this.original.getCombinedLight(pos, lightValue);
+    }
+
+    public BiomeGenBase getBiomeGenForCoords(BlockPos pos) {
+        return this.original.getBiomeGenForCoords(pos);
+    }
+
+    public boolean extendedLevelsInChunkCache() {
+        return this.original.extendedLevelsInChunkCache();
+    }
+
+    public WorldType getWorldType() {
+        return this.original.getWorldType();
+    }
+}
