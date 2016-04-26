fdb5e2d908ae9aa2ca1db332b95d939003946fcf
bloodshot <jdroque@gmail.com>
2016-04-26 13:56:27 -0400

    Fix compile issues.

--- src/main/java/org/spongepowered/mod/mixin/core/world/MixinChunk.java
+++ src/main/java/org/spongepowered/mod/mixin/core/world/MixinChunk.java
@@ -24,16 +24,22 @@
  */
 package org.spongepowered.mod.mixin.core.world;
 
+import com.flowpowered.math.vector.Vector3i;
 import net.minecraft.world.ChunkCoordIntPair;
 import net.minecraft.world.WorldServer;
 import net.minecraftforge.common.DimensionManager;
 import net.minecraftforge.common.ForgeChunkManager;
+import org.spongepowered.api.util.Direction;
 import org.spongepowered.api.util.annotation.NonnullByDefault;
 import org.spongepowered.api.world.Chunk;
 import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import org.spongepowered.common.interfaces.IMixinChunk;
+import org.spongepowered.common.interfaces.world.gen.IMixinChunkProviderServer;
 
 @NonnullByDefault
 @Mixin(net.minecraft.world.chunk.Chunk.class)
@@ -60,4 +66,17 @@ public abstract class MixinChunk implements Chunk, IMixinChunk {
         return true;
     }
 
+    @Inject(method = "setChunkLoaded", at = @At("RETURN"))
+    public void onSetChunkLoaded(boolean loaded, CallbackInfo ci) {
+        Direction[] directions = {Direction.NORTH, Direction.SOUTH, Direction.EAST, Direction.WEST};
+        for (Direction direction : directions) {
+            Vector3i neighborPosition = this.getPosition().add(direction.toVector3d().toInt());
+            net.minecraft.world.chunk.Chunk neighbor = ((IMixinChunkProviderServer) this.worldObj.getChunkProvider()).getChunkIfLoaded
+                    (neighborPosition.getX(), neighborPosition.getZ());
+            if (neighbor != null) {
+                this.setNeighbor(direction, (Chunk) neighbor);
+                ((IMixinChunk) neighbor).setNeighbor(direction.getOpposite(), this);
+            }
+        }
+    }
 }
