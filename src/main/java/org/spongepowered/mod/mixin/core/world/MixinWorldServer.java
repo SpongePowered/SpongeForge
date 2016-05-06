1581391c7a481ac78c3400bb6b2dfd0276000ede
Chris Sanders <zidane@spongepowered.org>
2016-05-05 23:52:09 -0500

    Restore SF dimension ids on Worlds.
    Signed-off-by: Chris Sanders <zidane@spongepowered.org>

--- src/main/java/org/spongepowered/mod/mixin/core/world/MixinWorldServer.java
+++ src/main/java/org/spongepowered/mod/mixin/core/world/MixinWorldServer.java
@@ -1,8 +1,11 @@
 package org.spongepowered.mod.mixin.core.world;
 
+import net.minecraft.world.WorldProvider;
 import net.minecraft.world.WorldServer;
 import org.spongepowered.api.world.World;
+import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.common.interfaces.world.IMixinWorldServer;
 import org.spongepowered.common.world.gen.SpongeChunkGenerator;
 import org.spongepowered.common.world.gen.SpongeWorldGenerator;
@@ -10,6 +13,17 @@ import org.spongepowered.mod.world.gen.SpongeChunkGeneratorForge;
 
 @Mixin(value = WorldServer.class, priority = 1001)
 public abstract class MixinWorldServer implements World, IMixinWorldServer {
+    @Shadow @Final public WorldProvider worldProvider;
+
+    @Override
+    public void setDimensionId(int dimensionId) {
+        throw new IllegalStateException("Sponge implementation should never set the dimension id, only Forge should!");
+    }
+
+    @Override
+    public int getDimensionId() {
+        return this.worldProvider.getDimension();
+    }
 
     @Override
     public SpongeChunkGenerator createChunkGenerator(SpongeWorldGenerator newGenerator) {
