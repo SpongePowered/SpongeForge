432f1ed75a73af911f28f5ee4ca084318abfa4cf
bloodshot <jdroque@gmail.com>
2016-06-09 15:33:10 -0400

    Fix client NPE during explosion event.

--- src/main/java/org/spongepowered/mod/mixin/core/event/world/MixinEventWorldExplosion.java
+++ src/main/java/org/spongepowered/mod/mixin/core/event/world/MixinEventWorldExplosion.java
@@ -63,6 +63,10 @@ public abstract class MixinEventWorldExplosion extends MixinEvent implements Exp
 
     @Inject(method = "<init>", at = @At("RETURN"))
     private void onConstruct(CallbackInfo callbackInfo) {
+        if (this.world.isRemote) {
+            return;
+        }
+
         if (this.explosion.exploder == null) {
             this.cause = Cause.of(NamedCause.source(this.world.getBlockState(new BlockPos(this.explosion.getPosition()))));
         } else {
@@ -111,6 +115,10 @@ public abstract class MixinEventWorldExplosion extends MixinEvent implements Exp
         @Inject(method = "<init>", at = @At("RETURN"))
         public void onConstructed(net.minecraft.world.World world, net.minecraft.world.Explosion explosion, List<Entity> entityList,
                 CallbackInfo ci) {
+            if (this.world.isRemote) {
+                return;
+            }
+
             createSpongeData();
         }
 
