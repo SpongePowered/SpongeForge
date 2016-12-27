a02b2559ebda713277cb2d5a40e840ae2477274a
Minecrell <dev@minecrell.net>
2016-12-27 11:57:45 +0100

    Update to Minecraft 1.11.2

--- src/java6/java/org/spongepowered/mod/SpongeCoremod.java
+++ src/java6/java/org/spongepowered/mod/SpongeCoremod.java
@@ -40,7 +40,7 @@ import java.io.File;
 import java.lang.reflect.Field;
 import java.util.Map;
 
-@IFMLLoadingPlugin.MCVersion("1.11")
+@IFMLLoadingPlugin.MCVersion("1.11.2")
 public class SpongeCoremod implements IFMLLoadingPlugin {
 
     public static final class TokenProvider implements IEnvironmentTokenProvider {
