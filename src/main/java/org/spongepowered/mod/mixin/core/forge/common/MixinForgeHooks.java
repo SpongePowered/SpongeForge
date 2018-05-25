c47bef205be2bd9a8fca67534de7b46de3b4b747
Gabriel Harris-Rouquette <gabizou@me.com>
2018-05-24 18:29:44 -0700

    Add another pre-block break check with MixinForgeHooks. This should bring maximum compatibility with Chisel n Bits as far as protections are concerned.
    Signed-off-by: Gabriel Harris-Rouquette <gabizou@me.com>

--- src/main/java/org/spongepowered/mod/mixin/core/forge/common/MixinForgeHooks.java
+++ src/main/java/org/spongepowered/mod/mixin/core/forge/common/MixinForgeHooks.java
@@ -24,15 +24,33 @@
  */
 package org.spongepowered.mod.mixin.core.forge.common;
 
+import net.minecraft.block.Block;
+import net.minecraft.block.state.IBlockState;
 import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.item.ItemStack;
 import net.minecraft.util.EnumActionResult;
 import net.minecraft.util.EnumHand;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.IBlockAccess;
+import net.minecraft.world.World;
 import net.minecraftforge.common.ForgeHooks;
 import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.event.ForgeEventFactory;
 import net.minecraftforge.event.entity.player.PlayerInteractEvent;
+import org.spongepowered.api.Sponge;
+import org.spongepowered.api.event.CauseStackManager;
+import org.spongepowered.api.event.block.ChangeBlockEvent;
+import org.spongepowered.api.event.cause.EventContextKeys;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Overwrite;
+import org.spongepowered.common.event.SpongeCommonEventFactory;
+import org.spongepowered.common.interfaces.world.IMixinWorld;
+import org.spongepowered.common.interfaces.world.IMixinWorldServer;
+import org.spongepowered.common.item.inventory.util.ItemStackUtil;
+import org.spongepowered.common.util.ServerUtils;
 import org.spongepowered.mod.interfaces.IMixinEventBus;
+
+import javax.annotation.Nonnull;
 
 @Mixin(value = ForgeHooks.class, remap = false)
 public abstract class MixinForgeHooks {
@@ -51,5 +69,59 @@ public abstract class MixinForgeHooks {
         PlayerInteractEvent.RightClickItem evt = new PlayerInteractEvent.RightClickItem(player, hand);
         final boolean post = ((IMixinEventBus) MinecraftForge.EVENT_BUS).post(evt, true);
         return post ? evt.getCancellationResult() : null;
+    }
+
+    /**
+     * @author gabizou - May 24th, 2018
+     * @reason Add procussive {@link ChangeBlockEvent.Pre} events to be thrown here
+     * as mods can use this hook to check if a block can be broken. Specifically
+     * however, this does not interact with {@link ForgeEventFactory#doPlayerHarvestCheck(EntityPlayer, IBlockState, boolean)}
+     * since that can potentially be a byproduct of {@link EntityPlayer#canHarvestBlock(IBlockState)}.
+     * This assures that Sponge will be able to at the very least interact with the event pre-emptively
+     * if some hook elsewhere fails.
+     *
+     * @param block
+     * @param player
+     * @param world
+     * @param pos
+     * @return
+     */
+    @Overwrite
+    public static boolean canHarvestBlock(@Nonnull Block block, @Nonnull EntityPlayer player, @Nonnull IBlockAccess world, @Nonnull BlockPos pos)
+    {
+        IBlockState state = world.getBlockState(pos);
+        state = state.getBlock().getActualState(state, world, pos);
+        if (state.getMaterial().isToolNotRequired())
+        {
+            return true;
+        }
+
+        ItemStack stack = player.getHeldItemMainhand();
+        String tool = block.getHarvestTool(state);
+        if (stack.isEmpty() || tool == null)
+        {
+            return player.canHarvestBlock(state);
+        }
+
+        // Sponge Start - Add the changeblockevent.pre check here before we bother with item stacks.
+        if (world instanceof IMixinWorldServer && !((IMixinWorld) world).isFake() && ServerUtils.isCallingFromMainThread()) {
+            try (CauseStackManager.StackFrame frame = Sponge.getCauseStackManager().pushCauseFrame()) {
+                // Might as well provide the active item in use.
+                frame.addContext(EventContextKeys.USED_ITEM, ItemStackUtil.snapshotOf(player.getActiveItemStack()));
+                if (SpongeCommonEventFactory.callChangeBlockEventPre((IMixinWorldServer) world, pos, player).isCancelled()) {
+                    // Since a plugin cancelled it, go ahead and cancel it.
+                    return false;
+                }
+            }
+        }
+        // Sponge End
+
+        int toolLevel = stack.getItem().getHarvestLevel(stack, tool, player, state);
+        if (toolLevel < 0)
+        {
+            return player.canHarvestBlock(state);
+        }
+
+        return toolLevel >= block.getHarvestLevel(state);
     }
 }
