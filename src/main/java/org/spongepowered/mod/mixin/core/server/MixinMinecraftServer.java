15bf39b1f4140bd2c465aae02e49e76d38477298
bloodshot <jdroque@gmail.com>
2018-07-20 16:44:15 -0400

    Update SpongeCommon for chunk gc fix. Fixes #2311

--- src/main/java/org/spongepowered/mod/mixin/core/server/MixinMinecraftServer.java
+++ src/main/java/org/spongepowered/mod/mixin/core/server/MixinMinecraftServer.java
@@ -28,15 +28,18 @@ import net.minecraft.profiler.Snooper;
 import net.minecraft.server.MinecraftServer;
 import net.minecraft.world.WorldServer;
 import net.minecraftforge.common.DimensionManager;
-import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.spongepowered.api.Server;
 import org.spongepowered.api.world.ChunkTicketManager;
-import org.spongepowered.api.world.Dimension;
 import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Overwrite;
 import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.At.Shift;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import org.spongepowered.asm.mixin.injection.callback.LocalCapture;
 import org.spongepowered.common.SpongeImpl;
 import org.spongepowered.common.interfaces.IMixinMinecraftServer;
 import org.spongepowered.common.interfaces.world.IMixinWorldServer;
@@ -76,6 +79,14 @@ public abstract class MixinMinecraftServer implements Server, IMixinMinecraftSer
     @Override
     public ChunkTicketManager getChunkTicketManager() {
         return this.chunkTicketManager;
+    }
+
+    @Inject(method = "updateTimeLightAndEntities", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/WorldServer;updateEntities()V", shift = Shift.AFTER), locals = LocalCapture.CAPTURE_FAILHARD)
+    public void onPostUpdateEntities(CallbackInfo ci, Integer ids[], int x, int id, long i, WorldServer worldServer) {
+        IMixinWorldServer spongeWorld = (IMixinWorldServer) worldServer;
+        if (spongeWorld.getChunkGCTickInterval() > 0) {
+            spongeWorld.doChunkGC();
+        }
     }
 
     /**
