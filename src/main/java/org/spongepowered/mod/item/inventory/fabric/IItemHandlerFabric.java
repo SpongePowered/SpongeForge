7e71fbf81056663e2a6b8d4e3c28d2ba45de422e
Anselm Brehme <development.anselm@breh.me>
2018-09-22 15:56:16 +0200

    add IItemHandlerModifiable Support

--- src/main/java/org/spongepowered/mod/item/inventory/fabric/IItemHandlerFabric.java
+++ src/main/java/org/spongepowered/mod/item/inventory/fabric/IItemHandlerFabric.java
@@ -27,6 +27,7 @@ package org.spongepowered.mod.item.inventory.fabric;
 import com.google.common.collect.ImmutableSet;
 import net.minecraft.item.ItemStack;
 import net.minecraftforge.items.IItemHandler;
+import net.minecraftforge.items.IItemHandlerModifiable;
 import org.spongepowered.api.text.translation.FixedTranslation;
 import org.spongepowered.api.text.translation.Translation;
 import org.spongepowered.common.SpongeImpl;
@@ -91,6 +92,17 @@ public class IItemHandlerFabric implements Fabric {
         if (setStackUnsupported.contains(handler.getClass())) {
             return; // setting item is not always possible
         }
+
+        if (handler instanceof IItemHandlerModifiable) {
+            try {
+                ((IItemHandlerModifiable) handler).setStackInSlot(index, stack);
+            } catch (RuntimeException e) {
+                setStackUnsupported.add(handler.getClass());
+                SpongeImpl.getLogger().warn("Modded Inventory refused setting slot. Sponge cannot handle modified slot transactions for this type of Inventory. " + handler.getClass());
+            }
+            return;
+        }
+
         ItemStack prev = handler.getStackInSlot(index);
         if (!prev.isEmpty()) {
             int cnt = prev.getCount();
