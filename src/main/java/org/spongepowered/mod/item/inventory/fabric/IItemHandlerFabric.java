0c4b0d6df93e055c82e676bf8ac33069828f222f
Anselm Brehme <development.anselm@breh.me>
2018-09-29 13:50:42 +0200

    implement clear for modifiable IItemHandlers

--- src/main/java/org/spongepowered/mod/item/inventory/fabric/IItemHandlerFabric.java
+++ src/main/java/org/spongepowered/mod/item/inventory/fabric/IItemHandlerFabric.java
@@ -82,6 +82,11 @@ public class IItemHandlerFabric implements Fabric {
 
     @Override
     public void clear() {
+        if (this.inventory instanceof IItemHandlerModifiable) {
+            for (int i = 0; i < this.inventory.getSlots(); i++) {
+                ((IItemHandlerModifiable) this.inventory).setStackInSlot(i, ItemStack.EMPTY);
+            }
+        }
     }
 
     @Override
