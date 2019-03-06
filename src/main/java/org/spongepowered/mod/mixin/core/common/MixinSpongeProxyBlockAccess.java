0aca6b7bf5868804f32aa4645019be48f055c07e
Gabriel Harris-Rouquette <gabizou@me.com>
2019-03-06 14:21:09 -0800

    Small updates to get MultiBlockCaptureSupplier working along with changes for handling block restores.
    Signed-off-by: Gabriel Harris-Rouquette <gabizou@me.com>

--- src/main/java/org/spongepowered/mod/mixin/core/common/MixinSpongeProxyBlockAccess.java
+++ src/main/java/org/spongepowered/mod/mixin/core/common/MixinSpongeProxyBlockAccess.java
@@ -31,7 +31,7 @@ import net.minecraft.world.WorldType;
 import net.minecraft.world.biome.Biome;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
-import org.spongepowered.common.world.SpongeProxyBlockAccess;
+import org.spongepowered.common.event.tracking.context.SpongeProxyBlockAccess;
 
 @Mixin(value = SpongeProxyBlockAccess.class, remap = false)
 public abstract class MixinSpongeProxyBlockAccess implements IBlockAccess {
