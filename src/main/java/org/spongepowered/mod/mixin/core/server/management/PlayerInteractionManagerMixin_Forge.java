b96f3343193e3ebace20c005bcd5086efa1c9504
Gabriel Harris-Rouquette <gabizou@me.com>
2019-08-08 10:22:53 -0700

    Manually merge adding of the Player cause to PlayerInteractionManager#tryHarvest. Merges SpongePowered/SpongeCommon#2304.
    Signed-off-by: Gabriel Harris-Rouquette <gabizou@me.com>

---
+++ src/main/java/org/spongepowered/mod/mixin/core/server/management/PlayerInteractionManagerMixin_Forge.java
@@ -0,0 +1,66 @@
+/*
+ * This file is part of Sponge, licensed under the MIT License (MIT).
+ *
+ * Copyright (c) SpongePowered <https://www.spongepowered.org>
+ * Copyright (c) contributors
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+package org.spongepowered.mod.mixin.core.server.management;
+
+import net.minecraft.entity.player.EntityPlayerMP;
+import net.minecraft.server.management.PlayerInteractionManager;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.World;
+import org.spongepowered.api.Sponge;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+import org.spongepowered.common.SpongeImpl;
+import org.spongepowered.common.bridge.world.WorldBridge;
+
+@Mixin(PlayerInteractionManager.class)
+public class PlayerInteractionManagerMixin_Forge {
+
+    @Shadow public World world;
+    @Shadow public EntityPlayerMP player;
+
+    @Inject(method = "tryHarvestBlock", at = @At("HEAD"))
+    private void forgeImpl$capturePlayer(final BlockPos pos, final CallbackInfoReturnable<Boolean> cir) {
+        if (((WorldBridge) this.world).bridge$isFake()) {
+            return;
+        }
+        Sponge.getCauseStackManager().pushCause(this.player);
+    }
+
+    @Inject(method = "tryHarvestBlock", at = @At("RETURN"))
+    private void forgeImpl$unlockPlayer(final BlockPos pos, final CallbackInfoReturnable<Boolean> cir) {
+        if (((WorldBridge) this.world).bridge$isFake()) {
+            return;
+        }
+        final Object cause = Sponge.getCauseStackManager().popCause();
+        if (cause != this.player) {
+            SpongeImpl.getLogger().warn("tryHarvestBlock(): expected to pop " + this.player + " but got " + cause);
+            SpongeImpl.getLogger().warn("Remaining cause: " + Sponge.getCauseStackManager().getCurrentCause());
+        }
+    }
+
+}
